97d33d95c13b0ec607e7aee477f6d4104764c96e
Luis <luised0912@gmail.com>
2020-03-30 14:25:14 -0600

    Add
    creation of superuser and first data record

--- Proyecto/Apps/Usuarios/models.py
+++ Proyecto/Apps/Usuarios/models.py
@@ -14,29 +14,62 @@ class Cancion(models.Model):
     calificacion = models.DecimalField(max_digits=4, decimal_places=2, null=True)
     album = models.ForeignKey('Album', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{} - {} - {}".format(self.nombre, self.album, self.duracion)
+
+    class Meta:
+        verbose_name="Cancion"
+        verbose_name_plural="Canciones"
+
 class Album(models.Model):
     nombre = models.CharField(max_length=59, null=False)
-    duracion = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    duracion = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     fecha = models.DateField(auto_now=False, auto_now_add=False)
-    foto = models.ImageField(upload_to=images_path, max_length=100)
+    foto = models.ImageField(upload_to=images_path, max_length=100, blank=True)
+    genero = models.ForeignKey('Genero', on_delete=models.CASCADE)
+    disquera = models.ForeignKey('Disquera', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
 
 class Genero (models.Model):
     nombre = models.CharField(max_length = 30)
     descripcion = models.CharField(max_length = 100, null = True)
 
+    def __str__(self):
+        return self.nombre
+
 class Disquera (models.Model):
     nombre = models.CharField(max_length = 100)
-    direccion = models.CharField(max_length = 100, null = True)
+    direccion = models.CharField(max_length = 100, null = True, blank = True)
+
+    def __str__(self):
+        return self.nombre
 
 class Playlist(models.Model):
      nombre = models.CharField(max_length = 250)
      is_public = models.BooleanField()
      usuario = models.ForeignKey(User, on_delete = models.CASCADE)
 
+     def __str__(self):
+        return "{} - {}".format(self.nombre, self.usuario)
+
 class PlaylistCanciones(models.Model):
     playlist = models.ForeignKey('Playlist', on_delete = models.CASCADE)
     Cancion = models.ForeignKey('Cancion', on_delete = models.CASCADE)
 
+    def __str__(self):
+        return self.playlist
+
+    class Meta:
+        verbose_name="PlaylistCanciones"
+        verbose_name_plural="PlaylistCanciones"
+
+
 class UsuarioCanciones(models.Model):
     usuario = models.ForeignKey(User, on_delete = models.CASCADE)
     Cancion = models.ForeignKey('Cancion', on_delete = models.CASCADE)
+
+    class Meta:
+        verbose_name="UsuarioCanciones"
+        verbose_name_plural="UsuariosCanciones"
